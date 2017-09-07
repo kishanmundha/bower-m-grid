2ec2a5fa88a09452ed89a3ce1d3f1d065dfe1d30
Kishan Mundha <kishan.mundha@gmail.com>
2017-09-07 15:17:08 +0530

    Upgrade version

--- dist/m-grid.js
+++ dist/m-grid.js
@@ -12,16 +12,82 @@
 
     angular.module('m-grid.config', [])
 
-    .constant('mGridConfig', {
-        tableClass: 'table table-striped table-bordered m-grid-table',
-        thClass: 'm-grid-th',
-        footerClass: 'm-grid-footer',
-        displayLimitOptions: [
-            { 'text': 10, 'value': 10 },
-            { 'text': 20, 'value': 20 },
-            { 'text': 50, 'value': 50 },
-            { 'text': 100, 'value': 100 }
-        ]
+    .provider('mGridConfig', function () {
+        var config = {
+            tableClass: 'table table-striped table-bordered m-grid-table',
+            thClass: 'm-grid-th',
+            footerClass: 'm-grid-footer',
+            defaultPageLimit: 10,
+            displayLimitOptions: [
+                { 'text': 10, 'value': 10 },
+                { 'text': 20, 'value': 20 },
+                { 'text': 50, 'value': 50 },
+                { 'text': 100, 'value': 100 }
+            ]
+        };
+
+        this.setDefaultPageLimit = function (limit) {
+            config.defaultPageLimit = limit || 10;
+        };
+
+        this.setDisplayLimitOptions = function (limitOptions) {
+            if (!angular.isArray(limitOptions)) {
+                throw new Error('param limitOptions should be array');
+            }
+
+            if (limitOptions.length === 0) {
+                throw new Error('param limitOptions should have at least one record');
+            }
+
+            config.displayLimitOptions = [];
+            limitOptions.forEach(function (item) {
+                if (angular.isObject(item)) {
+                    config.displayLimitOptions.push({
+                        text: item.text,
+                        value: item.value
+                    });
+                } else {
+                    config.displayLimitOptions.push({
+                        text: item,
+                        value: +item
+                    });
+                }
+            });
+        };
+
+        this.setCssClass = function (classObj) {
+            if (!angular.isObject(classObj)) {
+                throw new Error('param classObj should be a object');
+            }
+            if (classObj.hasOwnProperty('table')) {
+                config.tableClass = classObj.table;
+            }
+            if (classObj.hasOwnProperty('th')) {
+                config.thClass = classObj.th;
+            }
+            if (classObj.hasOwnProperty('footer')) {
+                config.footerClass = classObj.footer;
+            }
+        };
+
+        this.appendCssClass = function (classObj) {
+            if (!angular.isObject(classObj)) {
+                throw new Error('param classObj should be a object');
+            }
+            if (classObj.hasOwnProperty('table')) {
+                config.tableClass += ' ' + classObj.table;
+            }
+            if (classObj.hasOwnProperty('th')) {
+                config.thClass += ' ' + classObj.th;
+            }
+            if (classObj.hasOwnProperty('footer')) {
+                config.footerClass += ' ' + classObj.footer;
+            }
+        };
+
+        this.$get = [function () {
+            return config;
+        }];
     });
 
     angular.module('m-grid.directive', ['m-grid.config'])
@@ -55,7 +121,7 @@
             // pagination option
             $scope.startFrom = 0;
             $scope.currentPage = 1;
-            $scope.displayLimit = 10;
+            $scope.displayLimit = ($scope.gridOptions.config || {}).defaultPageLimit || mGridConfig.defaultPageLimit;
 
             // options for async loading data
             $scope.gridData = {
